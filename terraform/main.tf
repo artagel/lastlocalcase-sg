03b9b21ccb5335597b6496a8a3b03fa04829e45b
artagel <dan@syssec.biz>
2021-03-27 19:39:29 +0800

    Update tf files.

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,13 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIASLFW6NHW5OLKC4XU"
+  secret_key = "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"
 }
 
 provider "aws" {
   region = "us-east-1"
   alias  = "aws_cloudfront"
+  access_key = "AKIASLFW6NHW5OLKC4XU"
+  secret_key = "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"
 }
